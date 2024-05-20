34fff1c9e4868e27cd37f4bfdcf8bdfde9e050f1
DangQuangVi <vidangquang1203@gmail.com>
2024-05-20 16:41:01 +0700

    remove log

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from app import app
-import logging
-from logging.handlers import RotatingFileHandler
-
-# Cấu hình logging
-formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-file_handler = RotatingFileHandler('log_request.log', maxBytes=10000, backupCount=10)
-file_handler.setFormatter(formatter)
-app.logger.addHandler(file_handler)
-app.logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)      
