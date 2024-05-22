91db02ee21015c5f30bb02d18b9688ebd7101f44
coronatusvi <vidangquang1203@gmail.com>
2024-05-23 00:15:27 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -34,7 +34,7 @@ def scan_license_plate():
     filename = secure_filename(file.filename)
     file.save(filename)
 
-    # result = ""
-    # if not result: 
-        # return jsonify({"data": ["System not working!"]})
-    return jsonify({"data": ["System not working!"]}) 
+    result = detection_license_plate(filename)
+    if not result: 
+        return jsonify({"data": ["Không nhận dạng được ảnh!"]})
+    return jsonify({"data": list(result)}) 
