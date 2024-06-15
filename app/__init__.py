a3b607f7b7400e982af5a08c91bc498671244a86
DangQuangVi <vidangquang1203@gmail.com>
2024-06-15 17:09:45 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ def image_to_serial_check():
 
     # Lấy các tệp ảnh từ yêu cầu
     image_files = request.files.getlist('fileImage')
+    return jsonify(image_files)
     if not image_files:
         return jsonify({"errorMessage": "No images provided"}), 400
 
@@ -35,7 +36,7 @@ def scan_license_plate():
     file = request.files['fileImage']
     filename = secure_filename(file.filename)
     file.save(filename)
-    
+
     result = detection_license_plate(filename)
     if not result: 
         return jsonify({"errorMessage": "Không nhận dạng được ảnh!"}), 400
